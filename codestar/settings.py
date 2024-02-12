40e67cf7ed6bfafc6cefc360771544f04916dc49
Caylin Dewey <caylindewey42@gmail.com>
2024-02-12 14:50:54 +0000

    Add a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-caylindewey-blog-1il5wuxm3d2.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -92,6 +92,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {

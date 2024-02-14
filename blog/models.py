56e75ed2b26415e85e6b531e0d2f619c003ad667
Caylin Dewey <caylindewey42@gmail.com>
2024-02-14 13:33:11 +0000

    Add Django homepage

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,24 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+   
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_author"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
-
-
-def my_blog(request):
-    return HttpResponse("Hello Blog!")
+class PostList(generic.ListView):
+    model = Post

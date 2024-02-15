96225b16d62b9e8a80dbffd6fceadf3712c1f986
Caylin Dewey <caylindewey42@gmail.com>
2024-02-15 13:23:58 +0000

    Add filters to admin panel

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    initial = True
+    initial = False
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),

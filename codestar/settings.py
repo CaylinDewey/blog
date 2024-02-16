45c2c0aa5b01b1e6314c461a08a138cf3f5582fd
Caylin Dewey <caylindewey42@gmail.com>
2024-02-16 12:36:29 +0000

    Create individual post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-caylindewey-blog-1il5wuxm3d2.ws-eu108.gitpod.io', '.herokuapp.com']
 

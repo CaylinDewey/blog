9a89d1ac7043a924d30edfb4b486c89de34f7cbd
Caylin Dewey <caylindewey42@gmail.com>
2024-02-20 14:46:42 +0000

    Add collaborator request form and functionality

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from .models import Post
 from .forms import CommentForm
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
